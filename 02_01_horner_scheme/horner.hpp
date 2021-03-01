7a9c400029bc0eb9859ab50bbd0e15eccc39c424
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2021-03-02 00:20:43 +0300

    2.1

---
+++ 02_01_horner_scheme/horner.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+#include <span>
+#include <vector>
+
+namespace horner
+{
+  template<class T>
+  void calculate(std::span<T> coeffs)
+  {
+    T free_elem = coeffs.back();
+    T tmp = 0;
+
+    int len = coeffs.size()-2; 
+    int n = len;
+    
+    std::vector<T> result;
+    result.resize(n);
+    for (auto& a_i : coeffs)
+    {
+      tmp = a_i + free_elem * tmp;
+      if(n < 1)
+      {
+        std::cout << "; remain = x";
+        if(tmp < 0) std::cout << "+" << abs(tmp);
+        else std::cout << "-" << tmp;
+        break;
+      }
+      result[len-n] = tmp;
+      
+      if(n < len)
+        std::cout << " + ";
+      
+      std::cout << tmp;
+      if(n == 1)
+      {
+        n--;
+        continue;
+      }
+      std::cout << "x";
+      
+      if(n > 2)
+        std::cout << "^" << n-1;
+      n--;
+    }
+    std::cout << std::endl;
+    
+    assert(check<T>(coeffs, std::span{result}, free_elem, tmp));
+  }
+  
+  template<class T>
+  bool check(std::span<T> expected, std::span<T> result, T divisor, T remain)
+  {
+    auto len = result.size() + 1;
+    divisor = -divisor;
+    
+    T previous_num = 0;
+    
+    for(int i = 0; i < len-1; ++i)
+    {
+      previous_num += result[i];
+      if(expected[i] != previous_num)
+        return false;
+      previous_num = result[i] * divisor;
+    }
+
+    if(previous_num + remain != expected[len-1])
+      return false;
+    
+    return true;
+  }
+  
+}
